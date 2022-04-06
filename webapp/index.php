5487050edda0099857679a43a9ee563a2ba610c6
HerrMrLuca <s2110238074@students.fh-hagenberg.at>
2022-04-06 19:19:54 +0200

    Add everything

---
+++ webapp/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Webshop</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="webshop/vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="webshop/vendor/twbs/bootstrap-icons/font/bootstrap-icons.css">
+</head>
+<body>
+<header>
+    <nav class="navbar navbar-light">
+        <div class="container-lg">
+            <a class="navbar-brand" href="/webshop/public">
+                <img src="webshop/views/images/fhooe.svg" alt="" height="30" class="d-inline-block align-text-top">
+                GoTo Webshop
+            </a>
+        </div>
+        <div class="container-lg">
+            <a class="navbar-brand" href="/webshop-solution/public">
+                <img src="webshop-solution/views/images/fhooe.svg" alt="" height="30" class="d-inline-block align-text-top">
+                GoTo Webshop Solution
+            </a>
+        </div>
+    </nav>
+</header>
+<main>
+    <div class="container-lg mt-lg-4">
+        <ul>
+            <li>
+                <a class="navbar-brand" href="/hyp2ue_regex">Regex</a>
+            </li>
+            <li>
+                <a class="navbar-brand" href="/hyp2ue_regex/example">Regex Example</a>
+            </li>
+            <li>
+                <a class="navbar-brand" href="/hyp2ue_solutions_mh/regex">Regex Solution</a>
+            </li>
+            <li>
+                <a class="navbar-brand" href="/hyp2ue_oo">OOPHP</a>
+            </li>
+            <li>
+                <a class="navbar-brand" href="/hyp2ue_oo/examples/call_methods.php">OOPHP Examples</a>
+            </li>
+            <li>
+                <a class="navbar-brand" href="/hyp2ue_solutions_mh/oophp">Solution OOPHP</a>
+            </li>
+        </ul>
+    </div>
+</main>
+<div class="container-lg">
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <div class="col-lg-9 d-flex align-items-center">
+            <img src="webshop/views/images/fhooe.svg" alt="" height="24" class="me-2">
+            <span class="text-muted">© FH Oberösterreich | Department of Digital Media</span>
+        </div>
+        <ul class="nav col-lg-3 justify-content-end list-unstyled d-flex">
+            <li class="ms-3">
+                <a class="text-muted" href="https://github.com/Digital-Media/fhooe-router-skeleton">
+                    <i class="bi-github"></i>
+                </a>
+            </li>
+        </ul>
+    </footer>
+</div>
+</body>
+</html>
